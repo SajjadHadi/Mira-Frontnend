1cb89f50364e348e7f710f0930236466fa4b625e
SajjadHadi <sajjadhadi77@gmail.com>
2025-04-17 13:11:10 +0200

    Finish history page.

--- src/app/pages/history/history.component.ts
+++ src/app/pages/history/history.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { CommonModule, DatePipe } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { TableModule } from 'primeng/table';
+import { Statement } from '../../interfaces/statement';
+import { LlmService } from '../../services/llm.service';
+
+interface Column {
+  field: string;
+  header: string;
+}
 
 @Component({
   selector: 'app-history',
-  imports: [],
+  standalone: true,
+  imports: [TableModule, CommonModule, DatePipe],
   templateUrl: './history.component.html',
 })
-export class HistoryComponent {
+export class HistoryComponent implements OnInit {
+  statements: Statement[] = [];
+  loading = true;
+  cols: Column[];
 
+  constructor(private llmService: LlmService) {
+    const allCols: Column[] = [
+      { field: 'id', header: 'ID' },
+      { field: 'statement', header: 'Statement' },
+      { field: 'disorders', header: 'Detected Disorders' },
+      { field: 'created_at', header: 'Date' }
+    ];
+
+    // Filter out the ID column
+    this.cols = allCols.filter(col => col.field !== 'id');
+  }
+
+  ngOnInit() {
+    this.loadStatements();
+  }
+
+  loadStatements(page: number = 1, perPage: number = 10) {
+    this.loading = true;
+    this.llmService.getStatements(page, perPage).subscribe({
+      next: (response) => {
+        this.statements = response.statements;
+        this.loading = false;
+      },
+      error: (err) => {
+        console.error('Error loading statements:', err);
+        this.loading = false;
+      }
+    });
+  }
 }
