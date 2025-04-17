7bef812fcf42f8fd531eb144fac25875eb02c275
SajjadHadi <sajjadhadi77@gmail.com>
2025-04-17 08:56:03 +0200

    Config basic statement form

---
+++ src/app/interfaces/statement.ts
@@ -0,0 +1,8 @@
+export interface CreateStatement {
+  statement: string;
+}
+
+export interface Statement {
+  statement: string;
+  disorder: string;
+}
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { Validators } from "@angular/forms";
+import { FormComponent } from "../../components/form/form.component";
+import { CreateStatement } from "../../interfaces/statement";
 
 @Component({
     selector: 'app-dashboard',
-    imports: [],
+    imports: [
+        FormComponent
+    ],
     templateUrl: './dashboard.component.html',
 })
 export class DashboardComponent {
 
+    statementConfig = {
+        title: 'Mental Statement Deteails',
+        description: "Describe your emotional state as detailed as possible then click detect button to get the detection.",
+        fields: [
+            {
+                name: 'statement',
+                label: 'Statement',
+                type: 'textarea',
+                icon: 'pi-pen-to-square',
+                validators: [Validators.required, Validators.minLength(7), Validators.maxLength(255)],
+            },
+        ],
+        submitLabel: 'Detect'
+    }
+
+    onState(data: CreateStatement) {
+        console.log(data);
+    }
 }
